5fe3192f5b38b86b87d99ecf82d3e77ed4f17409
Jatin Prakash <jatinprakash1511@gmail.com>
2026-01-16 14:11:53 -0500

    update run command

--- submit_torch.sh
+++ submit_torch.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=/scratch/jp7467/slurm_logs/%j_%x.out
 #SBATCH --error=/scratch/jp7467/slurm_logs/%j_%x.err
 #SBATCH --export=ALL
-#SBATCH --time=12:00:00
+#SBATCH --time=30:00:00
 #SBATCH --gres=gpu:h200:1
 #SBATCH --account=torch_pr_235_cds
 #SBATCH --mem=100G
@@ -19,18 +19,19 @@ source ~/.bashrc
 conda activate
 conda activate ct
 
-python collect_trajectories_single_turn.py \
+python collect_trajectories_budget_forcing.py \
         --dataset bicycleman15/intellect_3_code_very_hard \
         --model Qwen/Qwen3-4B-Instruct-2507 \
         --backend vllm \
-        --num-problems 100 \
-        --num-samples 160 \
+        --num-problems 1000 \
+        --num-samples 32 \
+        --num-attempts 5 \
         \
         --fast-eval \
         --eval-workers 16 \
         --eval-batch-size 8 \
         --eval-timeout-s 1.0 \
-        --push-to-hub bicycleman15/1k_160_single_turn
+        --push-to-hub bicycleman15/1k_32_s1
 
 ################################################
 
